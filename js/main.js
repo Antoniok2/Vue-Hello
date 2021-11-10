8d72e9759f9d8f722e5b823c1acb30070038e7d4
Antoniok2 <antoniopiazza18@gmail.com>
2021-11-10 14:58:11 +0100

    aggiunto testo h1 utilizzando vue

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@
 // utilizzando i data.
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+var prova = new Vue({
+    el: '#benvenutoVue',
+    data: {
+        testoH1: 'Benvenuto nel tuo primo utilizzo del framework "Vue.JS".'
+    }
+
+})
